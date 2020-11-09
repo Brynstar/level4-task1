ea03c64e86dbedf105b4c69e6d22b5086d31958a
Bryn van Dam <Bryn.vanDam@nice.com>
2020-11-09 03:11:00 +0100

    Functions implemented

---
+++ app/src/main/java/com/example/madlevel4task1/ui/ShoppingListAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.madlevel4task1.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel4task1.R
+import com.example.madlevel4task1.model.Product
+import kotlinx.android.synthetic.main.item_shopping_list.view.*
+
+class ShoppingListAdapter(private val products: List<Product>) : RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun databind(product: Product) {
+            itemView.tvName.text = product.productName
+            itemView.tvQuantity.text = product.productQuantity.toString()
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_shopping_list, parent, false)
+        )
+
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(products[position])
+    }
+
+
+}
